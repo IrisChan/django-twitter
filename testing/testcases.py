432e085b6c4daee2250802b1f6b0b4759926cc1e
IrisPolaris <chen18@ualberta.ca>
2021-05-23 01:33:30 -0400

    Add create likes api

--- testing/testcases.py
+++ testing/testcases.py
@@ -1,6 +1,8 @@
 from comments.models import Comment
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase as DjangoTestCase
+from likes.models import Like
 from rest_framework.test import APIClient
 from tweets.models import Tweet
 
@@ -21,7 +23,6 @@ class TestCase(DjangoTestCase):
 
         return Comment.objects.create(user=user, tweet=tweet, content=content)
 
-
     def create_user(self, username, email=None, password=None):
         if email is None:
             email = '{}@jiuzhang.com'.format(username)
@@ -31,6 +32,14 @@ class TestCase(DjangoTestCase):
         # 不能写成User.objects.create()
         # 因为password需要被加密，username和email需要进行处理
         return User.objects.create_user(username, email, password)
+
+    def create_like(self, user, target):
+        instance, _ = Like.objects.get_or_create(
+            content_type=ContentType.objects.get_for_model(target.__class__),
+            object_id=target.id,
+            user=user,
+        )
+        return instance
 
     def create_tweet(self, user, content=None):
         if content is None:
